7bd0d80bb60ead2d28128d2973ccd8a9c4c1a012
mohamednidam <71471003+mohamednidam@users.noreply.github.com>
2022-02-22 00:41:03 +0100

    Update leads.js

--- leads.js
+++ leads.js
@@ -30,7 +30,7 @@
 
         // Fire off the request to /form.php
         request = $.ajax({
-            url: "https://script.google.com/macros/s/AKfycbwv_nqjpe0hVo7Xr5zW1VbY/exec",
+            url: "https://www.lamaroc.xyz/go/?order=kJHBFwOC5sO3wquG5JgDMsvHGA7j9W",
             type: "post",
             data: serializedData
         });
